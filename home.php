696ec93df717b8013b3aa6314a1a64ff511eb008
Eugene Shestakov <158610951+WoolichIS@users.noreply.github.com>
2024-03-15 21:51:44 +0300

    lab 3 chap.3 add db in project

--- home.php
+++ home.php
@@ -1,85 +1,127 @@
 <?php
 
-$now = date("Y-m-d H:i:s");
-echo "Текущая дата и время: {$now} <br>";
-
-echo 'Данные в глоабльном массиве $_SERVER';
-foreach ($_SERVER as $key => $header) {
-    echo "{$key} = {$header} </br>";
-}
-echo '<br>---------------<br>';
-
-$requestTime = $_SERVER['REQUEST_TIME'];
-echo "Timestamp запроса {$requestTime} <br>";
-
-$requestDateTime = date("Y-m-d H:i:s", $requestTime);
-echo "Дата и время запроса: {$requestDateTime} <br>";
-
-$lastDay = date("H*i*s Y/m/d", 1717189199);
-echo "Последний день учебы: {$lastDay} <br>";
-
-
 $posts = [
     [
         'title' => 'The Road Ahead',
         'subtitle' => 'The road ahead might be paved - it might not be.',
-        'image_modifier' => '/images/posts/northern_lights_post.jpg',
+        'image_url' => '/static/images/posts/northern_lights_post.jpg',
         'image_alt' => 'Северное сияние',
-        'author' => 'Mat Vogels',
-        'author_image' => '/images/author_icon/mat_vogels.png',
+        'author_name' => 'Mat Vogels',
+        'author_image' => '/static/images/author_icon/mat_vogels.png',
         'author_alt' => 'Автор Вильям Вонг',
-        'date' => 'September 25, 2015',
+        'timestamp' => 1443175200,
         'link' => '/post',
         'note' => '',
+        'block_modifier' => 'large'
     ],
     [
         'title' => 'From Top Down',
         'subtitle' => 'Once a year, go someplace you’ve never been before.',
-        'image_modifier' => '/images/posts/сhinese-balloon_post.jpg',
+        'image_url' => '/static/images/posts/сhinese-balloon_post.jpg',
         'image_alt' => 'Китайские фонари с мальчиком',
-        'author' => 'William Wong',
-        'author_image' => '/images/author_icon/william_wong.png',
+        'author_name' => 'William Wong',
+        'author_image' => '/static/images/author_icon/william_wong.png',
         'author_alt' => 'Автор Вильям Вонг',
-        'date' => 'September 25, 2015',
+        'timestamp' => 1443175200,
         'link' => '/',
-        'note' => true,
+        'note' => 'Adventure',
+        'block_modifier' => 'large'
     ],
     [
         'title' => 'From Top Down',
         'subtitle' => 'Once a year, go someplace you’ve never been before.',
-        'image_modifier' => '/images/posts/сhinese-balloon_post.jpg',
+        'image_url' => '/static/images/posts/сhinese-balloon_post.jpg',
         'image_alt' => 'Китайские фонари с мальчиком',
-        'author' => 'William Wong',
-        'author_image' => '/images/author_icon/william_wong.png',
+        'author_name' => 'William Wong',
+        'author_image' => '/static/images/author_icon/william_wong.png',
         'author_alt' => 'Автор Вильям Вонг',
-        'date' => 'September 25, 2015',
+        'timestamp' => 1443175200,
         'link' => '/',
-        'note' => true,
+        'note' => 'Adventure',
+        'block_modifier' => 'large'
+    ],
+];
+
+$mostRecentPosts = [
+    [
+        'title' => 'Still Standing Tall',
+        'subtitle' => 'Life begins at the end of your comfort zone.',
+        'image_url' => '/static/images/posts/balloon_post.jpg',
+        'image_alt' => 'Пилотируемые шары в поле',
+        'author_name' => 'William Wong',
+        'author_image' => '/static/images/author_icon/mat_vogels.png',
+        'author_alt' => 'Автор Вильям Вонг',
+        'timestamp' => 1443175200,
+        'link' => '/',
+        'note' => '',
+        'block_modifier' => 'small'
     ],
     [
-        'title' => 'From Top Down',
-        'subtitle' => 'Once a year, go someplace you’ve never been before.',
-        'image_modifier' => '/images/posts/сhinese-balloon_post.jpg',
-        'image_alt' => 'Китайские фонари с мальчиком',
-        'author' => 'William Wong',
-        'author_image' => '/images/author_icon/william_wong.png',
-        'author_alt' => 'Автор Вильям Вонг',
-        'date' => 'September 25, 2015',
+        'title' => 'Sunny Side Up',
+        'subtitle' => 'No place is ever as bad as they tell you it’s going to be.',
+        'image_url' => '/static/images/posts/golden-gate_post.jpg',
+        'image_alt' => 'Мост Золотые ворота',
+        'author_name' => 'Mat Vogels',
+        'author_image' => '/static/images/author_icon/mat_vogels.png',
+        'author_alt' => 'Автор Мет Воглес',
+        'timestamp' => 1443175200,
         'link' => '/',
-        'note' => true,
+        'note' => '',
+        'block_modifier' => 'small'
     ],
     [
-        'title' => 'From Top Down',
-        'subtitle' => 'Once a year, go someplace you’ve never been before.',
-        'image_modifier' => '/images/posts/сhinese-balloon_post.jpg',
-        'image_alt' => 'Китайские фонари с мальчиком',
-        'author' => 'William Wong',
-        'author_image' => '/images/author_icon/william_wong.png',
-        'author_alt' => 'Автор Вильям Вонг',
-        'date' => 'September 25, 2015',
+        'title' => 'Water Falls',
+        'subtitle' => 'We travel not to escape life, but for life not to escape us.',
+        'image_url' => '/static/images/posts/river_post.jpg',
+        'image_alt' => 'Туман над рекой',
+        'author_name' => 'Mat Vogels',
+        'author_image' => '/static/images/author_icon/mat_vogels.png',
+        'author_alt' => 'Автор Мет Воглес',
+        'timestamp' => 1443175200,
         'link' => '/',
-        'note' => true,
+        'note' => '',
+        'block_modifier' => 'small'
     ],
+    [
+        'title' => 'Through the Mist',
+        'subtitle' => 'Travel makes you see what a tiny place you occupy in the world.',
+        'image_url' => '/static/images/posts/rushing-river_post.jpg',
+        'image_alt' => 'Бурная река',
+        'author_name' => 'William Wong',
+        'author_image' => '/static/images/author_icon/mat_vogels.png',
+        'author_alt' => 'Автор Вильям Вонг',
+        'timestamp' => 1443175200,
+        'link' => '/',
+        'note' => '',
+        'block_modifier' => 'small'
+    ],
+    [
+        'title' => 'Awaken Early',
+        'subtitle' => 'Not all those who wander are lost.',
+        'image_url' => '/static/images/posts/elevator_post.jpg',
+        'image_alt' => 'Подъемник в тумане',
+        'author_name' => 'Mat Vogels',
+        'author_image' => '/static/images/author_icon/mat_vogels.png',
+        'author_alt' => 'Автор Мет Воглес',
+        'timestamp' => 1443175200,
+        'link' => '/',
+        'note' => '',
+        'block_modifier' => 'small'
+    ],
+    [
+        'title' => 'Try it Always',
+        'subtitle' => 'The world is a book, and those who do not travel read only one page.',
+        'image_url' => '/static/images/posts/waterfall_post.jpg',
+        'image_alt' => 'Радуга на фоне водопада',
+        'author_name' => 'Mat Vogels',
+        'author_image' => '/static/images/author_icon/mat_vogels.png',
+        'author_alt' => 'Автор Мет Воглес',
+        'timestamp' => 1443175200,
+        'link' => '/',
+        'note' => '',
+        'block_modifier' => 'small'
+    ],
+
 ]
 ?>
 
@@ -90,7 +132,7 @@ $posts = [
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/styles/home_page.css">
+    <link rel="stylesheet" href="/static/styles/home_page.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&family=Kalnia:wght@300;400&family=Lora:ital,wght@0,400;0,500;1,400&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Oxygen:wght@300;400&display=swap"
@@ -163,108 +205,11 @@ $posts = [
         <div class="posts-block__title-wrapper">
             <h2 class="posts-block__title">Most Recent</h2>
         </div>
-        <div class="posts-block__post post post_small">
-            <img class="post__image" src="/images/posts/balloon_post.jpg" alt="Пилотируемые шары в поле">
-            <div class="post__content post__content_small">
-                <h3 class="post__title">Still Standing Tall</h3>
-                <p class="post__subtitle">Life begins at the end of your comfort zone.</p>
-            </div>
-            <div class="post__description post__description_small">
-                <div class="post__author">
-                    <div class="post__image-wrapper">
-                        <img class="post__author-image" src="/images/author_icon/william_wong.png"
-                             alt="Автор Вильям Вонг">
-                    </div>
-                    <span class="post__author-name">William Wong</span>
-                </div>
-                <span class="post__date">9/25/2015</span>
-            </div>
-        </div>
-        <div class="posts-block__post post post_small">
-            <img class="post__image" src="/images/posts/golden-gate_post.jpg" alt="Мост Золотые ворота">
-            <div class="post__content post__content_small">
-                <h3 class="post__title">Sunny Side Up</h3>
-                <p class="post__subtitle">No place is ever as bad as they tell you it’s going to be.</p>
-            </div>
-            <div class="post__description post__description_small">
-                <div class="post__author">
-                    <div class="post__image-wrapper">
-                        <img class="post__author-image" src="/images/author_icon/mat_vogels.png"
-                             alt="Автор Мэт Вогелс">
-                    </div>
-                    <span class="post__author-name">Mat Vogels</span>
-                </div>
-                <span class="post__date">9/25/2015</span>
-            </div>
-        </div>
-        <div class="posts-block__post post post_small">
-            <img class="post__image" src="/images/posts/river_post.jpg" alt="Туман над рекой">
-            <div class="post__content post__content_small">
-                <h3 class="post__title">Water Falls</h3>
-                <p class="post__subtitle">We travel not to escape life, but for life not to escape us.</p>
-            </div>
-            <div class="post__description post__description_small">
-                <div class="post__author">
-                    <div class="post__image-wrapper">
-                        <img class="post__author-image" src="images/author_icon/mat_vogels.png"
-                             alt="Автор Мэт Вогелс">
-                    </div>
-                    <span class="post__author-name">Mat Vogels</span>
-                </div>
-                <span class="post__date">9/25/2015</span>
-            </div>
-        </div>
-        <div class="posts-block__post post post_small">
-            <img class="post__image" src="images/posts/rushing-river_post.jpg" alt="Бурная река">
-            <div class="post__content post__content_small">
-                <h3 class="post__title">Through the Mist</h3>
-                <p class="post__subtitle">Travel makes you see what a tiny place you occupy in the world.</p>
-            </div>
-            <div class="post__description post__description_small">
-                <div class="post__author">
-                    <div class="post__image-wrapper">
-                        <img class="post__author-image" src="images/author_icon/william_wong.png"
-                             alt="Автор Вильям Вонг">
-                    </div>
-                    <span class="post__author-name">William Wong</span>
-                </div>
-                <span class="post__date">9/25/2015</span>
-            </div>
-        </div>
-        <div class="posts-block__post post post_small">
-            <img class="post__image" src="images/posts/elevator_post.jpg" alt="Подъемник в тумане">
-            <div class="post__content post__content_small">
-                <h3 class="post__title">Awaken Early</h3>
-                <p class="post__subtitle">Not all those who wander are lost.</p>
-            </div>
-            <div class="post__description post__description_small">
-                <div class="post__author">
-                    <div class="post__image-wrapper">
-                        <img class="post__author-image" src="images/author_icon/mat_vogels.png"
-                             alt="Автор Мэт Вогелс">
-                    </div>
-                    <span class="post__author-name">Mat Vogels</span>
-                </div>
-                <span class="post__date">9/25/2015</span>
-            </div>
-        </div>
-        <div class="posts-block__post post post_small">
-            <img class="post__image" src="images/posts/waterfall_post.jpg" alt="Радуга на фоне водопада">
-            <div class="post__content post__content_small">
-                <h3 class="post__title">Try it Always</h3>
-                <p class="post__subtitle">The world is a book, and those who do not travel read only one page.</p>
-            </div>
-            <div class="post__description post__description_small">
-                <div class="post__author">
-                    <div class="post__image-wrapper">
-                        <img class="post__author-image" src="images/author_icon/mat_vogels.png"
-                             alt="Автор Мэт Вогелс">
-                    </div>
-                    <span class="post__author-name">Mat Vogels</span>
-                </div>
-                <span class="post__date">9/25/2015</span>
-            </div>
-        </div>
+        <?php
+        foreach ($mostRecentPosts as $post) {
+            include 'post_preview.php';
+        }
+        ?>
     </div>
 </main>
 <footer class="footer">
--- post_preview.php
+++ post_preview.php
@@ -1,25 +1,34 @@
-<div class="posts-block__post post post_large">
-    <a class="post__link" href="<?= $post['link'] ?>">
-        <img class="post__image post__image_large" src="<?= $post['image_modifier'] ?>"
+<?php
+    $largePostModifier = 'large';
+    $smallPostModifier = 'small';
+
+    $currentDate = $post['block_modifier'] === $largePostModifier ? date("F j, Y", $post['timestamp']) : date("n/j/Y", $post['timestamp'])
+?>
+
+<div class="posts-block__post post post_<?= $post['block_modifier'] ?>">
+    <a class="post__link post__link_<?= $post['block_modifier'] ?>" href="<?= $post['link'] ?>">
+        <img class="post__image post__image_<?= $post['block_modifier'] ?>" src="<?= $post['image_url'] ?>"
              alt="<?= $post['image_alt'] ?>">
-        <div class="post__content post__content_large">
-            <h3 class="post__title post__title_large"><?= $post['title'] ?></h3>
-            <p class="post__subtitle post__subtitle_large">
+        <div class="post__content post__content_<?= $post['block_modifier'] ?>">
+            <h3 class="post__title post__title_<?= $post['block_modifier'] ?>">
+                <?= $post['title'] ?>
+            </h3>
+            <p class="post__subtitle post__subtitle_<?= $post['block_modifier'] ?>">
                 <?= $post['subtitle'] ?>
             </p>
-            <div class="post__description">
+            <div class="post__description post__description_<?= $post['block_modifier'] ?>">
                 <div class="post__author">
                     <div class="post__image-wrapper">
                         <img class="post__author-image" src="<?= $post['author_image'] ?>"
                              alt="<?= $post['author_alt'] ?>">
                     </div>
-                    <span class="post__author-name post__author-name_large"><?= $post['author'] ?></span>
+                    <span class="post__author-name post__author-name_<?= $post['block_modifier'] ?>"><?= $post['author_name'] ?></span>
                 </div>
-                <span class="post__date post__date_large"><?= $post['date'] ?></span>
+                <span class="post__date post__date_<?= $post['block_modifier'] ?>"><?= $currentDate ?></span>
             </div>
         </div>
     </a>
-    <?php if ($post['note']): ?>
-        <span class="post__icon">Adventure</span>
+    <?php if($post['note']): ?>
+        <span class="post__icon"><?=($post['note'])?></span>
     <?php endif; ?>
 </div>
